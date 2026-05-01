9eab90f5bc9a0ed4bbb3567174d5a732703a50b8
Hussain Mushtaq <hussainmushtaq3636@gmail.com>
2026-05-01 11:52:21 +0500

    added the new regiester page and added the tailwindcss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Login from './pages/login'; // This imports the code you just pasted into login.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './pages/register'; 
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <Router>
+      <Routes>
+        {/* This makes Register the first thing you see at http://localhost:3000/ */}
+        <Route path="/" element={<Register />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
